27d2a675fcd123d5cdafa60da0818fda338bdaa2
zonkee <zonkee.l@outwitinc.com>
2014-10-31 18:11:17 +0800

    database backup

---
+++ data/fruit.sql
@@ -0,0 +1,39 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : fruit
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2014-10-31 18:10:12
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for fruit_admin_user
+-- ----------------------------
+DROP TABLE IF EXISTS `fruit_admin_user`;
+CREATE TABLE `fruit_admin_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `username` varchar(30) NOT NULL COMMENT '帐号',
+  `password` char(32) NOT NULL COMMENT '密码',
+  `real_name` varchar(20) NOT NULL COMMENT '真实姓名',
+  `email` varchar(255) DEFAULT NULL COMMENT '邮箱',
+  `add_time` int(11) NOT NULL COMMENT '添加时间',
+  `last_time` int(11) DEFAULT NULL COMMENT '上一次登录时间',
+  `type` tinyint(1) NOT NULL DEFAULT '0' COMMENT '类型（1：系统管理员，0：普通管理员）',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态(1：正常，0：禁用)',
+  `desc` varchar(255) DEFAULT NULL COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='后台管理员表';
+
+-- ----------------------------
+-- Records of fruit_admin_user
+-- ----------------------------
+INSERT INTO `fruit_admin_user` VALUES ('1', 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'admin', 'admin@admin.com', '0', '1414749596', '1', '1', '系统管理员，勿删！');
