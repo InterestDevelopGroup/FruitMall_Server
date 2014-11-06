559f39dfcb51ab8c3cfabcb2240b125dd81d4015
zonkee <zonkee.l@outwitinc.com>
2014-11-06 13:40:36 +0800

    add four apis
    validate_code
    register
    login
    find_password

--- app/Lib/Action/ApiAction.class.php
+++ app/Lib/Action/ApiAction.class.php
@@ -9,12 +9,76 @@
  */
 class ApiAction extends Action {
 
+    public function index() {
+        $this->ajaxReturn(array(
+            'status' => 0,
+            'result' => '未知错误'
+        ));
+    }
+
+    /**
+     * 找回密码
+     */
+    public function find_password() {
+        if ($this->isPost() || $this->isAjax()) {
+            $phone = isset($_POST['phone']) ? trim($_POST['phone']) : $this->redirect('/');
+            $new_password = isset($_POST['new_password']) ? trim($_POST['new_password']) : $this->redirect('/');
+            if (empty($phone) || empty($new_password)) {
+                $this->ajaxReturn(array(
+                    'status' => 0,
+                    'result' => '参数错误'
+                ));
+            }
+            $this->ajaxReturn(D('Member')->find_password($phone, $new_password));
+        } else {
+            $this->redirect('/');
+        }
+    }
+
+    /**
+     * 用户登录
+     */
+    public function login() {
+        if ($this->isPost() || $this->isAjax()) {
+            $phone = isset($_POST['phone']) ? trim($_POST['phone']) : $this->redirect('/');
+            $password = isset($_POST['password']) ? trim($_POST['password']) : $this->redirect('/');
+            if (empty($phone) || empty($password)) {
+                $this->ajaxReturn(array(
+                    'status' => 0,
+                    'result' => '参数错误'
+                ));
+            }
+            $this->ajaxReturn(D('Member')->login($phone, $password));
+        } else {
+            $this->redirect('/');
+        }
+    }
+
+    /**
+     * 用户注册
+     */
+    public function register() {
+        if ($this->isPost() || $this->isAjax()) {
+            $phone = isset($_POST['phone']) ? trim($_POST['phone']) : $this->redirect('/');
+            $password = isset($_POST['password']) ? trim($_POST['password']) : $this->redirect('/');
+            if (empty($phone) || empty($password)) {
+                $this->ajaxReturn(array(
+                    'status' => 0,
+                    'result' => '参数错误'
+                ));
+            }
+            $this->ajaxReturn(D('Member')->register($phone, $password));
+        } else {
+            $this->redirect('/');
+        }
+    }
+
     /**
      * 验证码
      */
     public function validate_code() {
         if ($this->isPost() || $this->isAjax()) {
-            $phone = isset($_POST['phone']) ? intval($_POST['phone']) : $this->redirect('/');
+            $phone = isset($_POST['phone']) ? trim($_POST['phone']) : $this->redirect('/');
             if (empty($phone)) {
                 $this->ajaxReturn(array(
                     'status' => 0,
